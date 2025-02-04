1e1ff02ac3d088aaf61e360cf0447da7aa32340c
Kasthuri K <140808049+Kasthurikittusamy@users.noreply.github.com>
2025-02-04 20:23:52 +0530

    Create JOptionFane.java

---
+++ JOptionFane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.java_crud;
+
+/**
+ *
+ * @author Admin
+ */
+class JOptionFane {
+
+    static void showMessageDialog(Student aThis, String record_Added) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
